416a8c322085d5a106e28f6f5a5ab1f487f5c863
vbjohnsen <vjo055@uit.no>
2022-04-12 03:31:03 +0200

    fragments

--- app/src/main/java/com/example/fotoalbum/fragments/UsersFragment.kt
+++ app/src/main/java/com/example/fotoalbum/fragments/UsersFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.fotoalbum.R
+import com.example.fotoalbum.API.MainViewModelFactory
+import com.example.fotoalbum.repository.Repository
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
+import com.example.fotoalbum.MainViewModel
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,26 +40,21 @@ class UsersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false)
+
+
+        val repository = Repository()
+        val ViewModelFactory = MainViewModelFactory(repository)
+        val viewModel = ViewModelProvider(this, ViewModelFactory)[MainViewModel::class.java]
+
+        viewModel.getUser()
+        viewModel.myUsersResponse.observe(requireActivity(), Observer { response ->
+            if (response.isSuccessful) {
+
+
+            }
+        })
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UsersFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UsersFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+    return binding.root
 }
